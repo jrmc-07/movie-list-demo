f1eb56fb06b6313fa522df1b1bf1ea03bc981c46
juan cornejo <juanrapmc@yahoo.com.ph>
2018-09-30 18:43:38 +0800

    Added form class for Movie model

---
+++ backend/movielistapp/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from movielistapp.models import Movie
+
+
+class MovieForm(forms.ModelForm):
+    """
+    Form class for input validation
+    """
+    class Meta:
+        model = Movie
+        fields = ('title')
