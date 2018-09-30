3d6642517a6b8e1715ba57989a59d175b175e533
juan cornejo <juanrapmc@yahoo.com.ph>
2018-09-30 23:55:45 +0800

    Added MovieTable component

---
+++ frontend/src/components/MovieTable.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="w3-container w3-center">
+        <h3>Movies</h3>
+        <table class="w3-table movie-table">
+            <thead>
+                <td class="w3-center"><b>Title</b></td>
+                <td class="w3-center"><b>Actions</b></td>
+            </thead>
+        </table>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    name: 'MovieTable',
+    data() {
+        return {
+            movies: [],
+        }
+    },
+    methods: {
+        async getMovies() {
+            const response = await axios.get('http://127.0.0.1:8000/movielist/movies');
+            this.movies = response.data;
+        }
+    },
+    mounted() {
+        this.getMovies();
+    }
+}
+</script>
+
+<style>
+.movie-table {
+    width: 50%;
+    margin-left: auto;
+    margin-right: auto;
+}
+</style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="w3-container">
+    <MovieTable></MovieTable>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import MovieTable from '@/components/MovieTable.vue'
 
 export default {
   name: 'home',
   components: {
+    MovieTable,
   }
 }
 </script>
