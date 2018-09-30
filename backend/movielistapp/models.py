9da961003cd5d4899005662bc9718dd2e65d86e3
juan cornejo <juanrapmc@yahoo.com.ph>
2018-09-30 18:05:21 +0800

    Adding movie model

--- backend/movielistapp/models.py
+++ backend/movielistapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Movie(models.Model):
+    """
+    Model to store movie data
+    """
+    title = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    likes = models.IntegerField()
+    date_created = models.DateField(auto_now_add=True)
+    date_updated = models.DateField(auto_now=True)
