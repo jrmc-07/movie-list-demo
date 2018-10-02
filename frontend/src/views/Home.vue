e3682342f5567b00060cb54f6dabede1a6208727
juan cornejo <juanrapmc@yahoo.com.ph>
2018-10-03 01:17:45 +0800

    Added frontend implementation for the welcome message

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="w3-container" style="height: 100%">
+    <div class="w3-container">
+      <h2 class="w3-left">{{ welcomeMessage }}</h2>
+    </div>
     <MovieTable class="mtable"></MovieTable>
     <button class="w3-button w3-round-large w3-border w3-right w3-blue" @click="addClicked">Add a Movie</button>
   </div>
@@ -7,6 +10,7 @@
 
 <script>
 // @ is an alias to /src
+import axios from 'axios'
 import MovieTable from '@/components/MovieTable.vue'
 
 export default {
@@ -14,10 +18,27 @@ export default {
   components: {
     MovieTable,
   },
+  data() {
+    return {
+      last_access_time: '0',
+    }
+  },
   methods: {
     addClicked() {
       this.$router.push('/movie/add');
+    },
+    async sessionGet() {
+      const response = await axios.get('http://127.0.0.1:8000/movielist/last_session', {withCredentials:true});
+      this.last_access_time = response.data;
     }
+  },
+  computed: {
+    welcomeMessage() {
+      return this.last_access_time == '0' ? "Welcome to our site!" : "Welcome back! You've visited this page last " + this.last_access_time;
+    }
+  },
+  mounted() {
+    this.sessionGet();
   }
 }
 </script>
